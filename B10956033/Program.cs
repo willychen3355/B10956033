18f41693a460d1acc9998be6e545150b0975892f
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:57:11 +0800

    3333

--- B10956033/Program.cs
+++ B10956033/Program.cs
@@ -6,7 +6,14 @@ namespace B10956033
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+
+            var total = meal_cost + tip_percent + tax_percent;
+
+            Console.WriteLine("總費用"+ total);
+
 
         }
     }
