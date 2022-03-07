4004ce7c5c76f78cf16f81a51f6b3ea81be391b5
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:57:02 +0800

    333

---
+++ B10956033/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace B10956033
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
